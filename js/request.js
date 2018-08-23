0f5f3addf0bb11c090594da20777de7bb5b65063
Alam Garcia <garcia76@avaya.com>
2018-08-23 09:04:42 -0500

    Se agrega ensor de emociones

--- js/request.js
+++ js/request.js
@@ -87,12 +87,36 @@ $("button#intencion").click(function() {
     }
   });
 
-  xhr.open("POST", "https://pruebas.aprimerpalo.com/speech/php/index.php");
+  xhr.open("POST", "https://pruebas.aprimerpalo.com/speech/php/intent.php");
   xhr.setRequestHeader("Cache-Control", "no-cache");
   xhr.setRequestHeader("Postman-Token", "24583018-557f-45ac-b166-728fd3f13f2a");
 
   xhr.send(data);
+//Segundo request
+var form2 = new FormData();
+form2.append("textoa", $("textarea#resultado").val());
 
+var settings2 = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://pruebas.aprimerpalo.com/speech/php/emociones.php",
+  "method": "POST",
+  "headers": {
+    "Cache-Control": "no-cache",
+    "Postman-Token": "696721c9-7447-4038-8a73-c04667bd8898"
+  },
+  "processData": false,
+  "contentType": false,
+  "mimeType": "multipart/form-data",
+  "data": form2
+}
+
+$.ajax(settings2).done(function (response2) {
+  var remociones = JSON.parse(response2);
+$("input#tono").val(remociones.document_tone.tones["0"].tone_name);
+
+
+});
 
 });
 
---
+++ php/emociones.php
@@ -0,0 +1,20 @@
+<?php
+function encodeURIComponent($str) {
+    $revert = array('%21'=>'!', '%2A'=>'*', '%27'=>"'", '%28'=>'(', '%29'=>')');
+    return strtr(rawurlencode($str), $revert);
+}
+$texto =  $_POST["textoa"];
+$texto = encodeURIComponent($texto);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21&text={$texto}");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+curl_setopt($ch, CURLOPT_USERPWD, "902d6228-8e62-4c1e-8aa3-cb81d6f55cc2" . ":" . "CBoYepyVoWjS");
+$result = curl_exec($ch);
+json_encode($result);
+echo $result;
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
+?>
